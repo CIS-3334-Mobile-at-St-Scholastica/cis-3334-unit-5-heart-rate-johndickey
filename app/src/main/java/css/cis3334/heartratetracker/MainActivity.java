4bba2769e2c5cc50ef934f4220b8d3bc62d5fc65
John <jdickey1@css.edu>
2018-02-22 13:23:02 -0800

    practice

--- app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
+++ app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
@@ -2,17 +2,30 @@ package css.cis3334.heartratetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    HeartRateList heartRateList;        // The list of heart rate objects
+    HeartRateList heartRateList;// The list of heart rate objects
+    ArrayAdapter<HeartRate> hrAdapter;
+    ListView lvHeartRates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvHeartRates = (ListView) findViewById(R.id.listViewHeartRate);
+
         heartRateList = new HeartRateList();
         heartRateList.InitRandomElderly();
+
+        hrAdapter = new HeartRateAdapter(this, R.layout.heart_rate_row, R.id.textviewpulse, heartRateList);
+        hrAdapter.setDropDownViewResource(R.layout.heart_rate_row);
+        lvHeartRates.setAdapter(hrAdapter);
     }
 }
